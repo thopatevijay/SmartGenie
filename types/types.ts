fc2bb690d228d2435e6a036173fd7e650ebb6cbe
thopatevijay <thopatevijay@gmail.com>
2024-06-23 13:41:44 +0530

    removing unneccessary codes

--- types/types.ts
+++ types/types.ts
@@ -1,17 +1,3 @@
-export type OpenAIModel = 'gpt-3.5-turbo' | 'gpt-4';
-
-export interface TranslateBody {
-  inputLanguage: string;
-  outputLanguage: string;
-  inputCode: string;
-  model: OpenAIModel;
-  apiKey: string;
-}
-
-export interface TranslateResponse {
-  code: string;
-}
-
 export interface Prompt {
   code: string
 }
