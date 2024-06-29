5f374ca07f09d1d28a0a9922899cfb21d413dcd7
thopatevijay <thopatevijay@gmail.com>
2024-06-29 16:24:10 +0530

    Wallet connected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { init, Web3OnboardProvider } from "@web3-onboard/react";
+import { onboardConfig } from "@/Providers";
+
+const wen3Onboard = init({
+  connect: {
+    autoConnectAllPreviousWallet: true,
+  },
+  ...onboardConfig,
+});
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />
+      <Web3OnboardProvider web3Onboard={wen3Onboard}>
+        <Component {...pageProps} />
+      </Web3OnboardProvider>
     </main>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { AIAgentTab, CodeReviewTab } from '@/components';
+import { useConnectWallet } from '@web3-onboard/react';
+import { WalletState } from "@web3-onboard/core/dist/types";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState('Code Review');
+  const [{ wallet, connecting }, connect] = useConnectWallet()
+  const [wallets, setWallets] = useState<WalletState>();
+
+  useEffect(() => {
+    if (wallet?.provider) {
+      console.log('Wallet connected:', wallet);
+      setWallets(wallet);
+    }
+  }, [wallet]);
+
+  const handleConnectClick = async () => {
+    try {
+      const walletStates = await connect();
+      console.log('Connected wallets:', walletStates);
+    } catch (error) {
+      console.error('Failed to connect wallet:', error);
+    }
+  };
+  const walletAddress = wallets?.accounts[0].address;
+
+  const truncateString = (str: string, length: number): string => {
+    return str.length > length ? `${str.slice(0, length / 2)}...${str.slice(-length / 2)}` : str;
+  };
+
+  const truncatedAddress = truncateString(walletAddress ?? '', 10);
+
   return (
     <div className="min-h-screen bg-darkbg text-neon font-mono flex flex-col">
       <Head>
         <title>SmartGenie</title>
       </Head>
-
-      <header className="flex justify-center py-4 border-b border-neon">
-        <h1 className="text-2xl">SmartGenie</h1>
+      <header className="flex justify-between items-center py-4 border-b border-neon">
+        <h1 className="text-2xl flex-1 text-center">SmartGenie</h1>
+        <button className="bg-neon text-darkbg border border-neon px-4 py-2 rounded-lg hover:bg-darkfg hover:text-neon transition duration-300 ease-in-out"
+          disabled={!!connecting || !!wallet?.provider}
+          onClick={handleConnectClick}
+        >
+          {walletAddress ? truncatedAddress : "Connect Wallet"}
+        </button>
       </header>
 
       <main className="flex-1 flex flex-col p-4">
